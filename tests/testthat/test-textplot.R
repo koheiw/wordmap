48a9f501276dcf970900a7318ee944540cb3ddbc
Kohei Watanabe <watanabe.kohei@gmail.com>
2024-07-15 07:49:06 +0900

    Add more tests

--- tests/testthat/test-textplot.R
+++ tests/testthat/test-textplot.R
@@ -5,17 +5,26 @@ dfmt_test <- dfm(toks_test) %>%
     dfm_remove(stopwords("en"))
 
 smat_test <- xtabs( ~ docid(dfmt_test) + dfmt_test$Party, sparse = TRUE)
-smat_test <- smat[,c("Republican", "Democratic")]
+smat_test <- smat_test[,c("Republican", "Democratic")]
 
 test_that("textplot_terms works with dictionary", {
 
-    map1 <- textmodel_wordmap(dfmt_test, smat_test)
+    wmp1 <- textmodel_wordmap(dfmt_test, smat_test)
 
     expect_silent(print(
-        textplot_terms(map1, data_dictionary_LSD2015, max_highlighted = 10)
+        textplot_terms(wmp1, data_dictionary_LSD2015, max_highlighted = 10)
     ))
     expect_silent(print(
-        textplot_terms(map1, dictionary(list(none = "xxxxx")))
+        textplot_terms(wmp1, dictionary(list(none = "xxxxx")))
+    ))
+
+    wmp2 <- textmodel_wordmap(dfmt_test, smat_test, entropy = "global")
+
+    expect_silent(print(
+        textplot_terms(wmp2, data_dictionary_LSD2015, max_highlighted = 10)
+    ))
+    expect_silent(print(
+        textplot_terms(wmp2, dictionary(list(none = "xxxxx")))
     ))
 })
 
